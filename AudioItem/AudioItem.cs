7419d6e59833e6d465cd1c62ff4623143991cb54
Liam Borella <liamborella26@gmail.com>
2023-08-26 15:13:59 +1000

    Added a basic scene that plays audio

---
+++ AudioItem/AudioItem.cs
@@ -0,0 +1,56 @@
+using Godot;
+using System;
+
+
+public partial class AudioItem : Node2D {
+	private float sampleHz;
+	private float pulseHz = 440;
+	private AudioStreamPlayer2D player;
+	private AudioStreamGenerator generator;
+	private AudioStreamGeneratorPlayback playback;
+
+	private Vector2[] buffer;
+
+	public override void _Ready() {
+		player = GetNode<AudioStreamPlayer2D>("AudioPlayer");
+		generator = (AudioStreamGenerator)player.Stream;
+
+		sampleHz = generator.MixRate;
+
+		PlaySound();
+	}
+
+	private float Decay(float progress) {
+		float velocity = 2.0f;
+		float decay = (float)Math.Exp(-velocity * progress);
+		if (progress < 0.5) {
+			return decay;
+		} else {
+			return (float)Mathf.Lerp(decay, 0, 2 * (progress - 0.5));
+		}
+	}
+
+	void PlaySound() {
+		player.Play();
+		FillBuffer();
+		Play();
+	}
+
+	private void FillBuffer() {
+		playback = (AudioStreamGeneratorPlayback)player.GetStreamPlayback();
+		float phase = 0;
+		float increment = pulseHz / sampleHz;
+		int framesAvailable = playback.GetFramesAvailable();
+		buffer = new Vector2[framesAvailable];
+
+		for (int i = 0; i < framesAvailable; i++) {
+			float progress = (float)i / (float)framesAvailable;
+			buffer[i] = Vector2.One * Decay(progress) * (float)Math.Sin(phase * Math.Tau);
+			phase = (phase + increment) % 1;
+		}
+	}
+
+	private void Play() {
+		playback.PushBuffer(buffer);
+	}
+}
